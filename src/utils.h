8665a7e9987bd64f0150746917546032982d9bd9
Carl Philipp Klemm <philipp@uvos.xyz>
2022-07-19 16:19:00 +0200

    utils: reformat header

--- src/utils.h
+++ src/utils.h
@@ -7,29 +7,31 @@
 
 bool pointInRect(const cv::Point2i& point, const cv::Rect& rect);
 
+bool pointIsOnLine(const cv::Point2i& point, const cv::Vec4f& line, double tollerance);
+
 std::pair<cv::Point2i, cv::Point2i> furthestPoints(std::vector<cv::Point2i> points);
 
 void deduplicatePoints(std::vector<cv::Point2i>& points, double tollerance);
 
-//taken fom opencv line detector src
-void drawLineSegments(cv::Mat& in, cv::InputArray lines, cv::Scalar color = cv::Scalar(0, 0, 255));
+double pointDist(const cv::Point2i& pointA, const cv::Point2i& pointB);
+
+
+void drawLineSegments(cv::Mat& in, cv::InputArray lines, cv::Scalar color = cv::Scalar(0, 0, 255)); //taken fom opencv line detector src
 
 double lineDotProd(const cv::Vec4f lineA, const cv::Vec4f lineB);
 
 double closestLineEndpoint(const cv::Vec4f lineA, const cv::Vec4f lineB);
 
-bool pointIsOnLine(const cv::Point2i& point, const cv::Vec4f& line, double tollerance);
-
-double pointDist(const cv::Point2i& pointA, const cv::Point2i& pointB);
-
-cv::Rect padRect(const cv::Rect& rect, double xPadPercent, double yPadPercent);
-
-cv::Mat getMatPlane(cv::Mat& in, int plane);
-
-cv::Mat getMatPlane4d(cv::Mat& in, int plane);
 
 std::string getMatType(const cv::Mat& mat);
 
 void printMatInfo(const cv::Mat& mat, const std::string& prefix = "", const Log::Level lvl = Log::INFO);
 
 void printMat(const cv::Mat& mat, const Log::Level lvl = Log::INFO);
+
+
+cv::Rect padRect(const cv::Rect& rect, double xPadPercent, double yPadPercent);
+
+cv::Mat getMatPlane(cv::Mat& in, int plane);
+
+cv::Mat getMatPlane4d(cv::Mat& in, int plane);
