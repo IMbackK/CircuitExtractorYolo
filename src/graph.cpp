ef1ac442e5ad2ee5bcf624047a886ead62448383
Carl Philipp Klemm <philipp@uvos.xyz>
2022-08-26 11:18:13 +0200

    add missing graph soruce file

---
+++ src/graph.cpp
@@ -0,0 +1,12 @@
+#include "graph.h"
+
+std::vector<std::pair<double, double>>& Graph::getPoints()
+{
+	//TODO
+	return points;
+}
+
+Graph::Graph(const cv::Mat& imageI, float probI, cv::Rect rectI): image(imageI), prob(probI), rect(rectI)
+{
+
+}
---
+++ src/graph.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <string>
+#include <utility>
+#include <vector>
+
+class Graph
+{
+private:
+	float prob;
+	cv::Mat image;
+	cv::Rect rect;
+	std::vector<std::pair<double, double>> points;
+
+public:
+	Graph() = default;
+	Graph(const cv::Mat& imageI, float probI, cv::Rect rectI);
+	void setImage(cv::Mat& imageI) {image = imageI;}
+	cv::Mat getImage() {return image;}
+	void setRect(const cv::Rect& rectI) {rect = rectI;}
+	cv::Rect getRect() {return rect;}
+	void setProb(float in) {prob = in;}
+	float getProb() {return prob;}
+	std::vector<std::pair<double, double>>& getPoints();
+};
