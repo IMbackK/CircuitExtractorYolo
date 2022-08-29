667cf6830396e2c3dd28c1e139fafdfdc150a988
Carl Philipp Klemm <philipp@uvos.xyz>
2022-08-29 12:04:51 +0200

    make graph data one-class

--- scripts/makeOneclass.sh
+++ scripts/makeOneclass.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 for file in $1/*txt; do
-	echo $file
-	sed 's/^./0/g' "$file" > "$file.out"
-	rm "$file"
-	mv "$file.out" "$file"
+	if ! [[ "$file" == *"classes.txt" ]]; then
+		echo $file
+		sed 's/^./0/g' "$file" > "$file.out"
+		rm "$file"
+		mv "$file.out" "$file"
+	fi
 done;
